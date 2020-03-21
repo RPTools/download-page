be6334d198ae9b6daee2f187f4613242c1289d93
OverCaffeinatedMonkey <OverCaffeinatedMonkey.github.com>
2020-03-22 02:03:29 +1030

    Updates

---
+++ precache-manifest.152a2c7e28939475d18d8d0467b2fc3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea356598af6895047267250ca8c8445",
+    "url": "/downloads/index.html"
+  },
+  {
+    "revision": "c2b55afe9a08b5c92108",
+    "url": "/downloads/static/js/2.30f18d07.chunk.js"
+  },
+  {
+    "revision": "bf71ac297fcfe9a3d2d199a48fc3f60c",
+    "url": "/downloads/static/js/2.30f18d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295623ea6916886efb28",
+    "url": "/downloads/static/js/main.e32ec0f8.chunk.js"
+  },
+  {
+    "revision": "036d319baabb1cfb8035",
+    "url": "/downloads/static/js/runtime-main.979fe529.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages-test/precache-manifest.23735b53f82db03da58453598791bca0.js"
+  "/downloads/precache-manifest.152a2c7e28939475d18d8d0467b2fc3e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages-test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/downloads/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
